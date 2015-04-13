1dd2420a9eab04b96981cb40d645c5e59a9e33b8
unknown <dwo053@MUSAVA202540.cof.ds.capitalone.com>
2015-04-13 14:41:51 -0400

    Split index.html up into multiple pages

--- assets/js/section-loader.js
+++ assets/js/section-loader.js
@@ -1,7 +1,4 @@
-//$(function () {
-//    $("#menu-section").load("assets/html/menu-section.html");
-//});
-
-$(function () {
-    $("#home-section").load("assets/html/home-section.html");
-});
+$('#menu-section').load('assets/html/menu-section.html');
+$('#home-section').load('assets/html/home-section.html');
+$('#source-code-section').load('assets/html/source-code-section.html');
+$('#about-section').load('assets/html/about-section.html');
